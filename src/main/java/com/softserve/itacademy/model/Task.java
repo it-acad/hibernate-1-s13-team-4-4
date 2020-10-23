c3c490a0cd8e86771bc1ad932430d87d6040462d
KovachYuliia <70910178+KovachYuliia@users.noreply.github.com>
2020-10-23 20:15:35 +0300

    Update Task.java

--- src/main/java/com/softserve/itacademy/model/Task.java
+++ src/main/java/com/softserve/itacademy/model/Task.java
@@ -1,5 +1,60 @@
 package com.softserve.itacademy.model;
 
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.validator.constraints.Length;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+
+@Entity
+@Table(name = "tasks")
 public class Task {
-//
+
+    @Id
+    @GeneratedValue(generator = "sequence-generator")
+    @GenericGenerator(
+            name = "sequence-generator",
+            strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+            parameters = {
+                    @org.hibernate.annotations.Parameter(name = "sequence_name", value = "task_sequence"),
+                    @org.hibernate.annotations.Parameter(name = "initial_value", value = "1"),
+                    @org.hibernate.annotations.Parameter(name = "increment_size", value = "1")
+            }
+    )
+    private long id;
+
+    @Column(name= "name")
+    @Length(min =3, max= 200)
+    @NotBlank(message = "The task name cannot be empty")
+    private String name;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private Priority priority;
+
+    @JoinColumn(name="state_id", referencedColumnName = "id")
+    private State state;
+
+
+    public Task() {    }
+
+    public long getId() { return id; }
+
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
+
+    public Priority getPriority() { return priority; }
+
+    public void setPriority(Priority priority) { this.priority = priority; }
+
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", priority=" + priority +
+                '}';
+    }
 }
