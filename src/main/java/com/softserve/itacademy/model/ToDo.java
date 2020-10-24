d4b2d205f862cee58c645353354cc6b80e52bdd7
KovachYuliia <70910178+KovachYuliia@users.noreply.github.com>
2020-10-24 16:06:51 +0300

    Update ToDo.java

--- src/main/java/com/softserve/itacademy/model/ToDo.java
+++ src/main/java/com/softserve/itacademy/model/ToDo.java
@@ -2,7 +2,6 @@ package com.softserve.itacademy.model;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.GenericGenerator;
-
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -11,6 +10,66 @@ import java.util.List;
 import java.util.Set;
 
 
+
+@Entity
+@Table(name = "todos")
 public class ToDo {
-//TODO
+    @Id
+    @GeneratedValue(generator = "sequence-generator")
+    @GenericGenerator(
+            name = "sequence-generator",
+            strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+            parameters = {
+                    @org.hibernate.annotations.Parameter(name = "sequence_name", value = "todo_sequence"),
+                    @org.hibernate.annotations.Parameter(name = "initial_value", value = "1"),
+                    @org.hibernate.annotations.Parameter(name = "increment_size", value = "1")
+            }
+    )
+    private long id;
+
+    @NotBlank(message = "The title cannot be empty")
+    @Column(nullable = false, unique = true)
+    private String title;
+
+    @Column(name= "created_at", columnDefinition = "TIMESTAMP")
+    @NotNull
+    private LocalDateTime createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "owner_id", referencedColumnName= "id")
+    private User owner;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "todo_collaborator",
+            joinColumns = @JoinColumn(name = "todo_id"),
+            inverseJoinColumns = @JoinColumn(name = "collaborator_id"))
+    private List<User> collaborators;
+
+    @OneToMany(mappedBy = "todo", cascade = CascadeType.REMOVE)
+    private List<Task> tasks;
+
+
+    public ToDo() {  }
+
+    public long getId() { return id; }
+
+    public String getTitle() { return title; }
+
+    public void setTitle(String title) { this.title = title; }
+
+    public LocalDateTime getCreatedAt() { return createdAt; }
+
+    public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
+
+    public User getOwner() { return owner; }
+
+    public void setOwner(User owner) { this.owner = owner; }
+
+    @Override
+    public String toString() {
+        return "\nToDo{" +
+                "title='" + title + '\'' +
+                ", createdAt=" + createdAt +
+                               '}';
+    }
 }
