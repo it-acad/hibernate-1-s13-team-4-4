6e8dfe76772592e3d6b7bc2599cff32fb0491a81
KovachYuliia <70910178+KovachYuliia@users.noreply.github.com>
2020-10-24 23:14:00 +0300

    Update User.java

--- src/main/java/com/softserve/itacademy/model/User.java
+++ src/main/java/com/softserve/itacademy/model/User.java
@@ -1,12 +1,15 @@
 package com.softserve.itacademy.model;
 
 import org.hibernate.annotations.GenericGenerator;
+
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 
 
 @Entity
